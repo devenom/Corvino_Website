99aba6956dcce6e064cd7af764fb2e7ad465f185
arianon <arianon@outlook.com>
2016-07-21 21:56:34 -0400

    Update

---
+++ script.js
@@ -0,0 +1,17 @@
+function collapseNavbar() {
+    var $nav = $('nav');
+
+    if ($nav.offset().top > 10) {
+        $nav.removeClass('navbar-ontop');
+    } else {
+        $nav.addClass('navbar-ontop');
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+$(function() {
+    // Pad the splash page to accomodate for the navbar.
+    $('#home').css("padding-top", $('nav').height());
+});
