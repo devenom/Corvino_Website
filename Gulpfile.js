44ee08c973f21e4ff3e667caf79d840a7a326b67
arianon <arianon@outlook.com>
2016-07-23 23:43:35 -0400

    Add Gulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,31 @@
+var
+    gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    bs = require('browser-sync').create();
+
+gulp.task('styles', function() {
+    gulp
+    .src('sass/style.scss')
+    .pipe(sass({
+        includePaths: ['sass/']
+    }))
+    .pipe(autoprefixer({
+        browsers: ['> 1%']
+    }))
+    .pipe(gulp.dest('css/'))
+    .pipe(bs.stream());
+});
+
+gulp.task('browser-sync', function() {
+    bs.init({
+        server: "./"
+    });
+});
+
+gulp.task('default', ['styles', 'browser-sync'], function() {
+    gulp.watch('sass/*', ['styles']);
+    gulp.watch('index.html').on('change', bs.reload)
+});
+
